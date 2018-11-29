0e234b8578cbca922c24abe103d8be54ad541970
Julio_userEduardo <eduardo.galicia.canales.contractor@bbva.com>
2018-11-28 21:05:40 -0600

    Add LinkedList Creation

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -355,7 +355,6 @@ let rotationArray = [1, 2, 3, 4, 5]
 
 let mod = rotations % numberOfNumbers
     for rotation in 1...rotations {
-        
     }
 print(mod)
 
@@ -570,6 +569,7 @@ public class Node<Value>: CustomStringConvertible {
     }
 }
 
+print("\n\n---------------- Nodos ----------------")
 let node1 = Node(value: 1)
 let node2 = Node(value: 2)
 let node3 = Node(value: 3)
@@ -578,3 +578,133 @@ node1.next = node2
 node2.next = node3
 
 print(node1)
+
+
+
+public struct LinkedList<Value>: CustomStringConvertible {
+    
+    public var head: Node <Value>?
+    public var tail: Node <Value>?
+    
+    public init() {}
+    
+    public var isEmpty: Bool {
+        return head == nil
+    }
+    
+    public var description: String {
+        guard let head = head else { return "Empty List" }
+        return String(describing: head)
+    }
+    
+    public mutating func pop() -> Value? {
+        defer {
+            head = head?.next
+            if isEmpty {
+                tail = nil
+            }
+        }
+        return head?.value
+    }
+    
+    public mutating func push(_ value: Value) {
+        head = Node(value: value, next: head)
+        if tail == nil {
+            tail = head
+        }
+    }
+    
+    public mutating func append(_ value: Value) {
+        guard !isEmpty else {
+            push(value)
+            return
+        }
+        
+        tail!.next = Node(value: value)
+        tail = tail!.next
+    }
+    
+    public func node(at index: Int) -> Node<Value>? {
+        var currentNode     =   head
+        var currentIndex    =   0
+        
+        while currentNode != nil && currentIndex < index {
+            currentNode     =   currentNode!.next
+            currentIndex    +=  1
+        }
+        
+        return currentNode
+    }
+    
+    @discardableResult
+    public mutating func insert(_ value: Value, after node: Node<Value>) -> Node<Value> {
+        guard tail != nil else {
+            append(value)
+            return tail!
+        }
+        
+        node.next = Node(value: value, next: node.next)
+        return node.next!
+    }
+    
+    @discardableResult
+    public mutating func removeLast() -> Value? {
+        guard let head = head else { return nil }
+        guard head.next != nil else { return pop() }
+        
+        var prev    =   head
+        var current =   head
+        
+        while let next = current.next {
+            prev    =   current
+            current =   next
+        }
+        
+        prev.next = nil
+        tail = prev
+        
+        return current.value
+    }
+    
+    public mutating func remove(after node: Node<Value>) -> Value? {
+        defer {
+            if node.next === tail {
+                tail = node
+            }
+            node.next = node.next?.next
+        }
+        
+        return node.next?.value
+    }
+    
+}
+
+print("\n\n---------------- Linked List ----------------")
+var list = LinkedList<Int>()
+list.push(3)
+list.push(2)
+list.push(1)
+
+//list.append(1)
+//list.append(2)
+//list.append(3)
+
+print("Before inserting: \(list)")
+var middleNode = list.node(at: 1)
+list.insert(4, after: middleNode!)
+print("After inserting: \(list)")
+
+print("Before poping\(list)")
+list.pop()
+print("After poping\(list)")
+
+print("After removing last: \(list)")
+list.removeLast()
+print("Before removing last: \(list)")
+
+
+print("After removing at: \(list)")
+let node = list.node(at: 0)!
+list.remove(after: node)
+print("Before removing at: \(list)")
+
